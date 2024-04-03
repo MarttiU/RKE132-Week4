64955660c057fde41850f66c560a5dd6fa2c8f8b
MarttiU <martti.umbleja@gmail.com>
2024-04-03 20:56:12 +0300

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿Random rnd = new Random();
+
+int myRandomNum;
+
+int randomSum = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    myRandomNum = rnd.Next(0, 11);
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"Random sum total: {randomSum}");
+
+//int myRandomNum = rnd.Next(0, 11); //genereeritakse väärtusi 0-10
+
+//Console.WriteLine($"My random number is: {myRandomNum}");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true; //bool - true/false
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine("Arva number ära, sisesta number 1-3:");
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Õnnitlused sa võitsid!");
+        loopActive = false;
+    }
+    else
+    {
+        Console.WriteLine("Vale, proovi uuesti:");
+    }
+}
+Console.WriteLine("Have a nice day!");
